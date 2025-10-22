ea74c444f20ef42f1586cdd99471676972431bc0
Srikanth Bellamkonda <itsbellamkonda@gmail.com>
2025-10-21 22:49:33 -0400

    update: Rename database to monitoring

--- API/automation/FIX/importlp.py
+++ API/automation/FIX/importlp.py
@@ -4,7 +4,7 @@ import sys
 # InfluxDB connection parameters
 host = "localhost"  # Update with your InfluxDB host
 port = 8086  # Update with your InfluxDB port
-database = "telegraf"  # Update with your InfluxDB database name
+database = "monitoring"  # Update with your InfluxDB database name
 
 # Initialize the InfluxDB client
 client = InfluxDBClient(host=host, port=port,  database=database)
