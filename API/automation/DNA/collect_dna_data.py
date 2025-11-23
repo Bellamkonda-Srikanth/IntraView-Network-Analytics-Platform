5533388a45484ba5efab267f1b7f5c8acb3dcccd
Srikanth Bellamkonda <itsbellamkonda@gmail.com>
2025-11-23 08:55:34 -0500

    rename: Update database name to dna_metrics

--- API/automation/DNA/collect_dna_data.py
+++ API/automation/DNA/collect_dna_data.py
@@ -9,7 +9,7 @@ Uncomment #password = 'Cisco123!'
 # InfluxDB details
 influxdb_host = 'influxdb'
 influxdb_port = 8086
-influxdb_database = 'ciscodna'
+influxdb_database = 'dna_metrics'
 
 def get_network_health_data():
     # Authenticate with Cisco DNA Center
