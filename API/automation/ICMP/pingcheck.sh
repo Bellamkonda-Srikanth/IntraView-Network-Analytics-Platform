584d0b25307e3beb2fef4677755678e822003263
Srikanth Bellamkonda <itsbellamkonda@gmail.com>
2026-01-23 00:51:42 -0500

    feat: Add logging to ping results file

--- API/automation/ICMP/pingcheck.sh
+++ API/automation/ICMP/pingcheck.sh
@@ -1,4 +1,4 @@
 shebang #!/bin/bash
 while IFS= read -r line; do
-   ping -c4 -W1 $line >> /dev/null && echo "$line is up" || echo "$line is down"
+   ping -c4 -W1 $line >> /dev/null && echo "$line is up" || echo "$line is down" >> ping_results.log
 done < newdevices.csv
