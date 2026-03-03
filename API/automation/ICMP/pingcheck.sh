9d16c1172935125546e11559f56f5293ef50f0ed
Srikanth Bellamkonda <itsbellamkonda@gmail.com>
2026-03-03 11:22:35 -0500

    fix: Correct bash shebang

--- API/automation/ICMP/pingcheck.sh
+++ API/automation/ICMP/pingcheck.sh
@@ -1,4 +1,4 @@
-shebang #!/bin/bash
+#!/bin/bash
 while IFS= read -r line; do
    ping -c4 -W1 -s 64 $line >> /dev/null && echo "$line is up" || echo "$line is down" >> ping_results.log
 done < newdevices.csv
